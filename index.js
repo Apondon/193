fdfc028c8a76ff579a41fff0ea5a590c232ff215
apondon <apondon@126.com>
2020-10-09 16:18:45 +0800

    提交全部文件

---
+++ index.js
@@ -0,0 +1,11 @@
+// 单行注释   /** */多行注释
+
+// alert('hello !!!!') // 打印hello
+// alert('单行注释1 !!!!')
+// alert('单行注释2 !!!!')
+/*
+alert('多行注释1 !!!!')
+alert('多行注释2 !!!!') 
+*/
+document.write('你好啊')
+document.write('<div class="item3"></div>')
